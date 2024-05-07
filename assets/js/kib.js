f0f201342ad7bec360eb4c7411c35928eea86344
gabriel sales <gabrielhub16@gmail.com>
2024-05-07 13:29:48 -0300

    feat arquivos javascript de pedidos

--- assets/js/kib.js
+++ assets/js/kib.js
@@ -1,9 +1,6 @@
-
-
 export default function incrementKib(){
    const h1 = document.getElementById("number-kib");
    let numberString = h1.innerText; //Armazenando a o Texto do H1  => "String"
    let number = new Number(numberString);//CONVERSÃO DO TEXTO DO H1 PARA NUMÉRO
     h1.innerText=` ${number + 1}`; //SOMA A CADA CLICK
-  
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
-import incrementKib from "./index.js";
+import incrementKib from "./kib.js";
+
 const incrementKibImg = document.getElementById("incrementKibImg");
+
 
 //A CADA CLICK UMA INCREMENTAÇÃO NO NÚMERO
 incrementKibImg.addEventListener("click",incrementKib);
