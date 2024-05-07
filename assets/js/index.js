a43171bc93857758e4cf3f0b1db844b51ae5fb3b
gabriel sales <gabrielhub16@gmail.com>
2024-05-07 11:32:08 -0300

    feat incrementador do kib 1.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
 
 export default function incrementKib(){
    const h1 = document.getElementById("number-kib");
-   let numberString = h1.innerText; 
+   let numberString = h1.innerText; //Armazenando a o Texto do H1  => "String"
    let number = new Number(numberString);//CONVERSÃO DO TEXTO DO H1 PARA NUMÉRO
     h1.innerText=` ${number + 1}`; //SOMA A CADA CLICK
   
