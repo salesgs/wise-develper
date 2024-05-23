3e0118779a31f5baf04bf29e329fc48a6bf66eed
gabriel sales <gabrielhub16@gmail.com>
2024-05-22 22:38:23 -0300

    geração de senha versao 1.1

--- assets/js/generateNumber.js
+++ assets/js/generateNumber.js
@@ -5,7 +5,18 @@ function catchNumber(){
  const h1NumberGenerate = document.getElementById("numberGenerate").innerText;
  let newNumber = parseInt(h1NumberGenerate.slice(2));
  newNumber+=1;
- console.log(newNumber);
+ 
  document.getElementById("numberGenerate").innerText=`#0${newNumber}`;
  document.getElementById("janela_modal_checkout").style.display="none";
+ 
+ //ZERANDO NÚMERO DOS ITENS
+ document.getElementById("number-kib").innerText="0";
+ document.getElementById("number-coxinha").innerText="0";
+ document.getElementById("number-queijo").innerText="0";
+
+ //ZERANDO VALORES DOS PEDIDOS
+ document.getElementById("total-valor-kib").innerText="R$:0.00"
+ document.getElementById("total-valor-coxinha").innerText="R$:0.00";
+ document.getElementById("total-valor-queijo").innerText="R$:0.00";
+
 }
